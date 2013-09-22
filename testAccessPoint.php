75e7b707e1f1f46189083c098e38f6138035a92d
Matt Shine <matthew.shine@uqconnect.edu.au>
2013-09-22 12:44:24 +1000

    random

--- testAccessPoint.php
+++ testAccessPoint.php
@@ -2,8 +2,12 @@
 
 include_once 'Parser.php';
 
+/**
+ * Provides access to the Parser & associated methods for unit testing.
+ */
+
 $p = new Parser("test.html");
-$p->parse();
-$p->createParseTree();
+//$p->parse();
+//$p->createParseTree();
 
 ?>
